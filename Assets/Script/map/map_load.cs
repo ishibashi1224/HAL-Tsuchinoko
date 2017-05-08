5be83b96797c6605f63a3961e6308856ebf55a98
THS45029\GhostDXY <ghost.love.free@icloud.com>
2017-05-08 15:03:53 +0900

    マップローダーの作成（試作）

---
+++ Assets/Script/map/map_load.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map_load : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject enemy;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
