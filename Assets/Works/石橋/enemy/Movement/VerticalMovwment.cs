2ff1dfccfb61e666f393d695fad7ea1b66cc3136
Takumi Ishibashi <Takumi Ishibashi>
2017-06-02 15:46:07 +0900

    no message

--- Assets/Works/石橋/enemy/Movement/VerticalMovwment.cs
+++ Assets/Works/石橋/enemy/Movement/VerticalMovwment.cs
@@ -22,12 +22,12 @@ public class VerticalMovwment : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        //transform.root.position = new Vector3(pos.x, pos.y, pos.z + (Mathf.Sin(angle) * height));
+        transform.root.position = new Vector3(pos.x, pos.y, pos.z + (Mathf.Sin(angle) * height));
         //transform.root.position += new Vector3(0.0f, 0.0f, Mathf.Sin(angle) * height);
-        //angle += speed;
-
-        transform.root.position += Vector3.Normalize((transform.root.position + new Vector3(0.0f, 0.0f, Mathf.Cos(angle) * height)) - transform.root.position);
         angle += speed;
+
+        //transform.root.position += Vector3.Normalize((transform.root.position + new Vector3(0.0f, 0.0f, Mathf.Cos(angle) * height)) - transform.root.position);
+        //angle += speed;
 
         if (angle > Mathf.PI * 2)
         {
