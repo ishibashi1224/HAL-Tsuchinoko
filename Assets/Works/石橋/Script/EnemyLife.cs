b18bfe80315ec1f18668fdf43df4c15519587b70
Takumi Ishibashi <Takumi Ishibashi>
2017-06-17 10:05:40 +0900

    shaderのtest

--- Assets/Works/石橋/Script/EnemyLife.cs
+++ Assets/Works/石橋/Script/EnemyLife.cs
@@ -8,16 +8,20 @@ public class EnemyLife : MonoBehaviour
     private float Life = 0.0f;
     [SerializeField]
     private GameObject particle = null;
+    private float _Life = 0.0f;
+    private Material material = null;
 
     // Use this for initialization
     void Start()
     {
-
+        material = gameObject.GetComponent<Renderer>().materials[1];
+        _Life = Life;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        material.SetFloat("_Range", Life / _Life);
         if (Life <= 0)
         {
             Instantiate(particle, transform.position, Quaternion.identity);
