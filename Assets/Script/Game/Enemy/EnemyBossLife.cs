39541b2215dd5d7ae8b3657c09cb3f3a291c0ca9
Takumi Ishibashi <Takumi Ishibashi>
2017-06-22 17:40:17 +0900

    BossにLifeを追加

---
+++ Assets/Script/Game/Enemy/EnemyBossLife.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBossLife : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Life = 0.0f;
+    [SerializeField]
+    private GameObject particle = null;
+    private float _Life = 0.0f;
+    private Material material = null;
+    private bool use = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = gameObject.GetComponent<Renderer>().materials[1];
+        _Life = Life;
+        use = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        material.SetFloat("_Range", Life / _Life);
+        if (Life <= 0)
+        {
+            use = false;
+        }
+    }
+
+    public void SubLife(float Power)
+    {
+        Life -= Power;
+        if (Life <= 0) Life = 0;
+    }
+
+    public float GetLife()
+    {
+        return Life;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "bullet")
+        {
+            SubLife(10.0f);
+            Destroy(collider.gameObject);
+        }
+    }
+
+    public bool GetUse()
+    {
+        return use;
+    }
+}
