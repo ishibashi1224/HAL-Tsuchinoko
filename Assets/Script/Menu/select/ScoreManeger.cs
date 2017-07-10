5205848cc683c79284121f93e4987919481443f2
kimuramasaya <bbb5d2ded79m7m@gmail.com>
2017-07-10 14:11:13 +0900

    no message

--- Assets/Script/Menu/select/ScoreManeger.cs
+++ Assets/Script/Menu/select/ScoreManeger.cs
@@ -4,63 +4,23 @@ using UnityEngine;
 
 public class ScoreManeger : SingletonMonoBehaviourFast<ScoreManeger>
 {
-    //private bool ScoreUse;   //menu使用フラグ
-    //private static bool ScoreChange;
-    [SerializeField]
-    Canvas RankingCanvas = null;
-
     // Use this for initialization
-    void Start () {
-    //    ScoreUse = false;
-     //   ScoreChange = false;
+    void Start()
+    {
+        //    ScoreUse = false;
+        //   ScoreChange = false;
     }
-	
+
     // Update is called once per frame
     void Update()
     {
         if (MenuManager.instance.GetMode() == MenuManager.MenuModeEnum.SCORE)
         {
-            //...
-            //score処理
-            //if (MenuManager.instance.GetMode() == MenuManager.MenuModeEnum.SCORE)
-            if (MenuButton.GetButtonLeft()&& !Scroll.GetUse()) //右押したら
+            if (MenuButton.GetButtonLeft() && !Scroll.GetUse()) //右押したら
             {
                 MenuManager.instance.SetModeEnum(MenuManager.MenuModeEnum.MENU);
-                //TutorialManeger.instance.SetUse(false);
-                //TutorialManeger.instance.SetChange(true);
-                //MenuManager.instance.SetUse(true);
-                //ScoreChange = true;
                 return;
-                
             }
-
-            
         }
     }
-
-    /*public bool GetUse()
-    {
-        return ScoreUse;
-    }
-
-    public void SetUse (bool use)
-    {
-        ScoreUse = use;
-    }
-
-    public bool GetChange()
-    {
-        return ScoreChange;
-    }
-
-    public void ResetChange()
-    {
-        ScoreChange = false;
-    }
-
-    public void SetChange(bool use)
-    {
-        ScoreChange = use;
-    }
-    */
 }
--- Assets/Script/Menu/select/TutorialManeger.cs
+++ Assets/Script/Menu/select/TutorialManeger.cs
@@ -4,65 +4,20 @@ using UnityEngine;
 
 public class TutorialManeger : SingletonMonoBehaviourFast<TutorialManeger>
 {
-    //private static bool TutorialChange;
-    //private bool TutorialUse;      //メニュー使用フラグ
-
-    [SerializeField]
-    Canvas TutorialCanvas = null;
-
     // Use this for initialization
     void Start()
     {
-   //     TutorialUse = false;
-    //    TutorialChange = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(MenuManager.instance.GetMode() == MenuManager.MenuModeEnum.TUTORIAL)
-        //if (TutorialUse)
-        {
+        if (MenuManager.instance.GetMode() == MenuManager.MenuModeEnum.TUTORIAL)
             //Tutorial処理
-            if(MenuButton.GetButtonRight()&& !Scroll.GetUse()) //右押したら
+            if (MenuButton.GetButtonRight() && !Scroll.GetUse()) //右押したら
             {
-
-                //TutorialUse = false;
                 MenuManager.instance.SetModeEnum(MenuManager.MenuModeEnum.MENU);
-                //ScoreManeger.instance.SetUse(false);
-                //MenuManager.instance.SetUse(true);
-                //TutorialChange = true;
-                //ScoreManeger.instance.SetChange(true);
-
             }
-        }
-
-
     }
-
-
-   /* public bool GetUse()
-    {
-        return TutorialUse;
-    }
-
-    public void SetUse(bool use)
-    {
-        TutorialUse = use;
-    }
-
-    public bool GetChange()
-    {
-        return TutorialChange;
-    }
-
-    public void ResetChange()
-    {
-        TutorialChange = false;
-    }
-
-    public void SetChange(bool use)
-    {
-        TutorialChange = use;
-    }*/
 }
